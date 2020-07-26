00b86d57cb1820d2de917911245ae46ef28a99ec
Mateus Ranchuka <ranchukamateus@gmail.com>
2020-07-26 05:26:28 -0300

    MvcMovie | vs_04 | Criacao de uma Model de Filmes, criaccao do banco de dados EF core e criacao de um crud com sccafolld

---
+++ MvcMovie/Data/MvcMovieContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Data
+{
+    public class MvcMovieContext: DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+           : base(options)
+        {
+        }
+
+#pragma warning disable CS0246 // O nome do tipo ou do namespace "Movie" não pode ser encontrado (está faltando uma diretiva using ou uma referência de assembly?)
+        public DbSet<Movie> Movie { get; set; }
+#pragma warning restore CS0246 // O nome do tipo ou do namespace "Movie" não pode ser encontrado (está faltando uma diretiva using ou uma referência de assembly?)
+    }
+}
