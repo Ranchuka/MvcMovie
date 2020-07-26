049bf048646c6b27f2c0af1f712dba701a675a33
Mateus Ranchuka <ranchukamateus@gmail.com>
2020-07-26 01:37:12 -0300

    MvcMovie | vs_02 | Nesta versao foi explorada a passagens de parametros pela rota da url nao utilizando ainda o segmento de paramentros url  mas da rota do controller

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "Este é minha ação default...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name , int id=1)
+        {
+            //  return "Este é o metodo de Ação Welcome...";
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID is: {id}");
+        }
+    }
+}
